43741bb69ea2c5801f5e5af5b343b6feceb3aa34
Satyam Jha <154743842+jha06sm@users.noreply.github.com>
2024-05-13 13:52:10 +0530

    Create 3.string.cpp

---
+++ 3.string.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string fn,ln;
+    cout<<"enter first name:";
+    cin>>fn;
+    cout<<"enter second name:";
+    cin>>ln;
+    cout<<"welcome!"<<fn<<"  "<<ln<<" in the world of OOP'S"<<endl;
+    return 0;
+}
